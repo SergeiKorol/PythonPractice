2850103a34d37d4a69ba2c47e1bfa8a15272bf8d
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-04-13 22:15:30 +0400

    Созданы функции для чтения файлов

--- hw_3/script_book.py
+++ hw_3/script_book.py
@@ -2,7 +2,6 @@
 
 import json
 import csv
-from csv import DictReader
 
 # Импорт адресов файлов?
 
@@ -24,19 +23,54 @@ def read_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file) # вернётся список из словарей
 
-users = read_json('user.json')
-print(users)
-
 # Распределяем книги
-books = read_csv('books.csv')
-users = read_json('user.json')
+# Создаю итератор
+class BookIterator:
+    def __init__(self, books):
+        self.books = books
+        self.index = 0
 
-total_user = len(users)
-total_books = len(books)
+    def __iter__(self):
+        return self
 
-print(total_user)
-print(total_books)
+    def __next__(self):
+        if self.index < len(self.books):
+            book = self.books[self.index]
+            self.index += 1
+            return book
+        else:
+            raise StopIteration
+
+# Распределяю книги
+def distribute_books(users, books):
+    book_iterator = BookIterator(books)
+    num_users = len(users)
+
+    for i, user in enumerate(users):
+        user['books'] = []
+        for a in range(len(books) // num_users):
+            user['books'].append(next(book_iterator))
+
+        if i < len(books) % num_users:
+            user['books'].append(next(book_iterator))
+
 
 # записываем результаты в файл
+def write_json(data, file_path):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
 
 # Вызываем ранее созданные функции чтоб всё случилось
+
+def main():
+    # Чтение данных из файлов
+    users = read_json('user.json')
+    books = read_csv('books.csv')
+
+    # Распределение книг пользователям
+    distribute_books(users, books)
+
+    # Запись результатов в файл
+    write_json(users, 'result.json')
+
+main()
