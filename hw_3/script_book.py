baffc5fb8d7def0faa4d75c2d4ee1ef4b4f1fd5b
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-04-19 11:54:20 +0400

    Добавлен файл __init__ Исправлено классовое неравенство

--- hw_3/script_book.py
+++ hw_3/script_book.py
@@ -1,34 +1,33 @@
 import json
 import csv
 from typing import List, Dict
+from hw_3 import csv_books, json_user, json_result
 
 
-class Merge_csv_json:
-
-    def get_csv(self, path: str) -> List[Dict[str, any]]:
-        """Метод для чтения данных из CSV файла и преобразования их в список словарей"""
-        with open(path, newline='') as file:
-            reader = csv.DictReader(file)
-            return [
-                {"title": value["Title"],
-                 "author": value["Author"],
-                 "pages": int(value["Pages"]),
-                 "genre": value["Genre"]
-                } for value in reader
-            ]
+def get_csv(path: str) -> List[Dict[str, any]]:
+    """Метод для чтения данных из CSV файла и преобразования их в список словарей"""
+    with open(path, newline='') as file:
+        reader = csv.DictReader(file)
+        return [
+            {"title": value["Title"],
+            "author": value["Author"],
+            "pages": int(value["Pages"]),
+            "genre": value["Genre"]
+            } for value in reader
+        ]
 
 
-    def get_json(self, path: str) -> List[Dict[str, any]]:
-        """Метод для чтения данных из JSON файла и преобразования их в список словарей"""
-        with open(path) as file:
-            data = json.load(file)
-            return [
-                {"name": user_data["name"],
-                 "gender": user_data["gender"],
-                 "address": user_data["address"],
-                 "age": user_data["age"]
-                } for user_data in data
-            ]
+def get_json(path: str) -> List[Dict[str, any]]:
+    """Метод для чтения данных из JSON файла и преобразования их в список словарей"""
+    with open(path) as file:
+        data = json.load(file)
+        return [
+            {"name": user_data["name"],
+            "gender": user_data["gender"],
+            "address": user_data["address"],
+            "age": user_data["age"]
+            } for user_data in data
+        ]
 
 
 def distribute_books(users, books):
@@ -50,17 +49,16 @@ def distribute_books(users, books):
 """записываем данные в файл"""
 
 def main():
-    merge = Merge_csv_json()
 
     # Чтение данных из файлов
-    users = merge.get_json('user.json')
-    books = merge.get_csv('books.csv')
+    users = get_json(json_user)
+    books = get_csv(csv_books)
 
     # Распределение книг пользователям
     distribute_books(users, books)
 
     # Запись результатов в файл
-    with open('result.json', 'w') as file:
+    with open(json_result, 'w') as file:
         json.dump(users, file, indent=4)
 
 if __name__ == "__main__":
