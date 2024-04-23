58648ac8515f0e5ae9cb5520cf95220d3e3f258a
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-04-24 01:27:38 +0400

    Тесты для dog.ceo

---
+++ hw_4/1_dog_ceo/test_file.py
@@ -0,0 +1,71 @@
+import requests
+from jsonschema import validate
+import pytest
+
+
+# Random image
+def test_json_schema_random_image():
+    # Выполняем GET-запрос к API
+    response = requests.get('https://dog.ceo/api/breeds/image/random')
+
+    # Определяем ожидаемую структуру ответа
+    expected_schema = {
+        "type": "object",
+        "properties": {
+            "message": {"type": "string"},
+            "status": {"type": "string"}
+        },
+        "required": ["message", "status"]
+    }
+
+    # Проверяем соответствие ответа ожидаемой схеме
+    validate(instance=response.json(), schema=expected_schema)
+
+
+# By breed
+@pytest.mark.parametrize("breed", ["chihuahua", "poodle", "labrador"])
+def test_status_code_by_breed(breed):
+    # Выполняем GET-запрос к API
+    response = requests.get(f"https://dog.ceo/api/breed/{breed}/images")
+    # Проверяем ответ через assert
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+
+
+# By sub-breed
+@pytest.mark.parametrize("breed", ["chihuahua", "poodle", "labrador"])
+def test_status_code_by_sub_breed(breed):
+    # Выполняем GET-запрос к API
+    response = requests.get(f"https://dog.ceo/api/breed/{breed}/list")
+    # Проверяем ответ через assert
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+
+
+# List all breeds
+def test_json_schema_list_all_breeds():
+    # Выполняем GET-запрос к API
+    response = requests.get('https://dog.ceo/api/breeds/list/all')
+
+    # Определяем ожидаемую структуру ответа
+    expected_schema = {
+        "type": "object",
+        "properties": {
+            "message": {"type": "object"},
+            "status": {"type": "string"}
+        },
+        "required": ["message", "status"]
+    }
+
+    # Проверяем соответствие ответа ожидаемой схеме
+    validate(instance=response.json(), schema=expected_schema)
+
+
+# Browse breed list
+@pytest.mark.parametrize("breed", ["chihuahua", "poodle", "labrador"])
+def test_status_code_breed_list(breed):
+    # Выполняем GET-запрос к API
+    response = requests.get(f"https://dog.ceo/api/breed/{breed}/images/random")
+    # Проверяем ответ через assert
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
