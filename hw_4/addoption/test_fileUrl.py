b62798288ec6e72b62b02afb2da750a743d164d4
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-05-02 01:21:36 +0400

    Параметр URL заработал

--- hw_4/addoption/test_fileUrl.py
+++ hw_4/addoption/test_fileUrl.py
@@ -1,23 +1,28 @@
 import pytest
 import requests
-import sys
-
 
 def pytest_addoption(parser):
-    parser.addoption('--url', action="store", default="https://ya.ru", help="URL to test")
-    parser.addoption('--status_code', action="store", default=200, type=int, help="Expected status code")
+    parser.addoption(
+        "--url",
+        default="https://httpbin.org/",
+        help="This is request url"
+    )
 
+    parser.addoption(
+        "--status_code",
+        default=200,
+        type=int,
+        help="Expected status code"
+    )
 
 @pytest.fixture
-def url(request):
-    return request.config.getoption('--url')
-
+def base_url(request):
+    return request.config.getoption("--url")
 
 @pytest.fixture
 def status_code(request):
-    return request.config.getoption('--status_code')
+    return request.config.getoption("--status_code")
 
-
-def test_check_status(url, status_code):
-    response = requests.get(url)
-    assert response.status_code == status_code, f"Expected status code {status_code}, got {response.status_code}"
+def test_check_status(base_url, status_code):
+    response = requests.get(base_url)
+    assert response.status_code == status_code, f"Expected status code {status_code}, but got {response.status_code}"
