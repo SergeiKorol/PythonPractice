80f934ada4038c08f3851fa9761ced4f840e7d34
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-05-17 21:29:28 +0400

    1 Убрал Try\except 2 Добавил Док стринги

--- hw_5/Pages/component_monitor.py
+++ hw_5/Pages/component_monitor.py
@@ -1,10 +1,3 @@
-# Наименование товара //h1
-# Цена товара без скидки //h2/span[@class="price-new"]
-# Значёк добавить в избранное $$('.fa-solid fa-heart')
-# Значёк сравнение //button[contains(@aria-label, "Compare this Product")]
-# Заголовок Available Options //div[@id="product"]/form/h3
-# Кнопка добавить в корзину //button[@id="button-cart"]
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,14 +5,17 @@ import time
 
 
 class MonitorPage:
+    """
+    Класс для работы со страницей одного из товаров монитор и элементами на этой странице
+    """
     url = "http://localhost"
-    #url = "http://localhost/en-gb/product/desktops/apple-cinema"
 
     # Локаторы
     item_name_locator = (By.XPATH, '//h1')
     item_price_locator = (By.XPATH, '//h2/span[@class="price-new"]')
-    wishlist_button_locator = (By.CSS_SELECTOR, '.fa-solid fa-heart')
-    #compare_button_locator = (By.XPATH, "//button[@aria-label='Compare this Product']")
+    wishlist_button_locator = (By.CSS_SELECTOR, "button[aria-label='Add to Wish List']")
+    upload_button_locator = (By.ID, "button-upload-222")
+    textarea_locator = (By.ID, "input-option-209")
     compare_button_locator = (By.CSS_SELECTOR, "button[aria-label='Compare this Product']")
     text_Available_Options_locator = (By.XPATH, '//div[@id="product"]/form/h3')
     add_to_cart_button_locator = (By.XPATH, '//button[@id="button-cart"]')
@@ -29,61 +25,60 @@ class MonitorPage:
         self._driver.maximize_window()
         self._driver.get(self.url)
 
-    # Методы для проверки наличия элементов
     def does_item_name_exist(self):
-        try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.item_name_locator))
-            return True
-        except:
-            return False
+        """
+        Метод для проверки наличия элемента с именем товара и получения его CSS стиля color
+        """
+        item_name = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.item_name_locator))
+        return item_name.value_of_css_property("color")
 
     def does_item_price_exist(self):
-        try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.item_price_locator))
-            return True
-        except:
-            return False
+        """
+        Метод для проверки наличия элемента с ценой товара и получения его CSS стиля color
+        """
+        item_price = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.item_price_locator))
+        return item_price.value_of_css_property("color")
 
-    def does_wishlist_button_exist(self):
-        try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.wishlist_button_locator))
-            return True
-        except:
-            return False
+    def does_upload_button_exist(self):
+        """
+        Метод для проверки наличия элемента для загрузки картинки товара и получения его CSS стиля color
+        """
+        upload_button = WebDriverWait(self._driver, 2).until(
+            EC.visibility_of_element_located(self.upload_button_locator))
+        return upload_button.value_of_css_property("color")
 
-    def does_compare_button_exist(self):
-        try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.compare_button_locator))
-            return True
-        except:
-            return False
+    def does_textarea_exist(self):
+        """
+        Метод для проверки наличия элемента для описания товара и получения его CSS стиля color
+        """
+        textarea = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.textarea_locator))
+        return textarea.value_of_css_property("color")
 
     def does_text_Available_Options_exist(self):
-        try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.text_Available_Options_locator))
-            return True
-        except:
-            return False
+        """
+        Метод для проверки наличия элемента для описания Available_Options товара и получения его CSS стиля color
+        """
+        text_Available_Options = WebDriverWait(self._driver, 2).until(
+            EC.visibility_of_element_located(self.text_Available_Options_locator))
+        return text_Available_Options.value_of_css_property("color")
 
     def does_add_to_cart_button_exist(self):
-        try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.add_to_cart_button_locator))
-            return True
-        except:
-            return False
+        """
+        Метод для проверки наличия элемента добавления в корзину товара и получения его CSS стиля color
+        """
+        add_to_cart_button = WebDriverWait(self._driver, 2).until(
+            EC.visibility_of_element_located(self.add_to_cart_button_locator))
+        return add_to_cart_button.value_of_css_property("color")
 
     def move_to_monitor(self):
-        self._driver.implicitly_wait(10)
+        """
+        Метод для перехода из главной страницы в описание конкретного товара монитор
+        """
         components_link = self._driver.find_element(By.LINK_TEXT, "Components")
         components_link.click()
-        self._driver.implicitly_wait(10)
-        time.sleep(3)
-        self._driver.implicitly_wait(10)
+        time.sleep(1)
         monitors_link = self._driver.find_element(By.LINK_TEXT, "Monitors (2)")
         monitors_link.click()
-        self._driver.implicitly_wait(10)
-        time.sleep(3)
+        time.sleep(1)
         apple_link = self._driver.find_element(By.LINK_TEXT, 'Apple Cinema 30"')
         apple_link.click()
-        self._driver.implicitly_wait(10)
-
--- hw_5/tests/test_component_monitor.py
+++ hw_5/tests/test_component_monitor.py
@@ -1,35 +1,59 @@
 from selenium import webdriver
-from Pages.component_monitor import MonitorPage
+from hw_5.Pages.component_monitor import MonitorPage
 
 driver = webdriver.Chrome()
 monitor_page = MonitorPage(driver)
 
 
 def test_item_name():
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(68, 68, 68, 1)'
     monitor_page.move_to_monitor()
-    assert monitor_page.does_item_name_exist()
+    assert monitor_page.does_item_name_exist() == expected_css
 
 
 def test_item_price():
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(68, 68, 68, 1)'
     monitor_page.move_to_monitor()
-    assert monitor_page.does_item_price_exist()
+    assert monitor_page.does_item_price_exist() == expected_css
 
 
-def test_wishlist_button():
+def test_upload_button():
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(119, 119, 119, 1)'
     monitor_page.move_to_monitor()
-    assert monitor_page.does_wishlist_button_exist()
+    assert monitor_page.does_upload_button_exist() == expected_css
 
 
-def test_compare_button():
+def test_textarea():
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(33, 37, 41, 1)'
     monitor_page.move_to_monitor()
-    assert monitor_page.does_compare_button_exist()
+    assert monitor_page.does_textarea_exist() == expected_css
 
 
 def test_text_Available_Options():
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(68, 68, 68, 1)'
     monitor_page.move_to_monitor()
-    assert monitor_page.does_text_Available_Options_exist()
+    assert monitor_page.does_text_Available_Options_exist() == expected_css
 
 
 def test_add_to_cart_button():
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(255, 255, 255, 1)'
     monitor_page.move_to_monitor()
-    assert monitor_page.does_add_to_cart_button_exist()
+    assert monitor_page.does_add_to_cart_button_exist() == expected_css
