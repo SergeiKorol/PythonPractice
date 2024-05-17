34e6b6d2d80c2a9ad93d76ec41c6e4fb255149bc
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-05-17 21:01:33 +0400

    1 Убрал Try\except 2 Добавил Док стринги

--- hw_5/Pages/admin.py
+++ hw_5/Pages/admin.py
@@ -4,6 +4,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class AdminPage():
+    """
+    Класс для работы со страницей каталог и элементами на этой странице
+    """
     url = "http://localhost/administration/"
 
     # Локаторы
@@ -20,36 +23,37 @@ class AdminPage():
 
     # Методы для проверки наличия элементов
     def does_username_input_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.username_input_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента username и получения его CSS стиля color
+        """
+        username = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.username_input_locator))
+        return username.value_of_css_property("color")
 
     def does_password_input_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.password_input_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента password и получения его CSS стиля color
+        """
+        password = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.password_input_locator))
+        return password.value_of_css_property("color")
 
     def does_login_button_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.login_button_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента login_button и получения его CSS стиля color
+        """
+        login_button = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.login_button_locator))
+        return login_button.value_of_css_property("color")
 
     def does_promo_link_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.promo_link_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента promo_link и получения его CSS стиля color
+        """
+        promo_link = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.promo_link_locator))
+        return promo_link.value_of_css_property("color")
 
     def does_data_input_form_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.data_input_form_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента data_input_form и получения его CSS стиля color
+        """
+        data_input = WebDriverWait(self._driver, 2).until(
+            EC.visibility_of_element_located(self.data_input_form_locator))
+        return data_input.value_of_css_property("color")
