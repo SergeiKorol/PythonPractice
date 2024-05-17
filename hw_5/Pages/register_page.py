a89af4077012eece1285f722dcf184b8dabc6020
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-05-17 13:09:01 +0400

    1 Для registerPage добавил логику перехода по вкладкам

--- hw_5/Pages/register_page.py
+++ hw_5/Pages/register_page.py
@@ -1,23 +1,30 @@
-# Поле ввода First Name //div/input[@placeholder="First Name"]
-# Поле ввода Last Name //div/input[@placeholder="Last Name"]
-# Кнопка Continue //div[@class="text-end"]/button
-# Ссылка на Privacy Policy //div[@class="text-end"]/div/label
-# Чекбокс Subscribe //div/input[@type="checkbox"]
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 class RegisterPage:
-    url = "http://localhost/index.php?route=account/register"
+    url = "http://localhost"
+    #url = "http://localhost/index.php?route=account/register"
 
     # Локаторы
     firstname_input_locator = (By.XPATH, '//div/input[@placeholder="First Name"]')
     lastname_input_locator = (By.XPATH, '//div/input[@placeholder="Last Name"]')
-    continue_button_locator = (By.XPATH, '//div[@class="text-end"]/button')
+    #continue_button_locator = (By.XPATH, '//div[@class="text-end"]/button')
     link_Privacy_Policy_locator = (By.XPATH, '//div[@class="text-end"]/div/label')
     checkbox_Subscribe_locator = (By.XPATH, '//div/input[@type="checkbox"]')
+    #my_account_locator = (By.CSS_SELECTOR, '.d-none d-md-inline')
+    #my_account_locator = (By.XPATH, "//li/div/a[@class='dropdown-toggle']")
+    # my_account_locator = (By.CSS_SELECTOR, 'a.dropdown - toggle[data - bs - toggle = "dropdown"]')
+    # my_account_locator = (By.LINK_TEXT, "My Account")
+    # my_account_locator = (By.XPATH,'//a[@class="dropdown-toggle" and @data-bs-toggle="dropdown"]')
+    my_account_locator = (By.XPATH, "//a[contains(@href,'route=account/account')]")
+    continue_button_locator = (By.XPATH, "//button[@type='submit']")
+    register_locator = (By.XPATH, '//a[@class="dropdown-toggle show" and @data-bs-toggle="dropdown" and @aria-expanded="true"]')
+
+
+
 
     def __init__(self, driver):
         self._driver = driver
@@ -27,35 +34,48 @@ class RegisterPage:
     # Методы для проверки наличия элементов
     def does_firstname_input_exist(self):
         try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.firstname_input_locator))
+            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.firstname_input_locator))
             return True
-        except:
-            return False
+        except Exception as e:
+            return e
 
     def does_lastname_input_exist(self):
         try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.lastname_input_locator))
+            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.lastname_input_locator))
             return True
-        except:
-            return False
+        except Exception as e:
+            return e
 
     def does_continue_button_exist(self):
         try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.continue_button_locator))
+            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.continue_button_locator))
             return True
-        except:
-            return False
+        except Exception as e:
+            return e
 
     def does_link_Privacy_Policy_exist(self):
         try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.link_Privacy_Policy_locator))
+            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.link_Privacy_Policy_locator))
             return True
-        except:
-            return False
+        except Exception as e:
+            return e
 
     def does_checkbox_Subscribe_exist(self):
         try:
-            WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located(self.checkbox_Subscribe_locator))
+            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.checkbox_Subscribe_locator))
             return True
-        except:
-            return False
+        except Exception as e:
+            return e
+
+    def move_my_account_dropdown(self):
+        self._driver.implicitly_wait(10)
+        my_account_link = self._driver.find_element(By.LINK_TEXT, "My Account")
+        my_account_link.click()
+        self._driver.implicitly_wait(10)
+        time.sleep(3)
+        self._driver.implicitly_wait(10)
+        register_link = self._driver.find_element(By.LINK_TEXT, "Register")
+        register_link.click()
+        self._driver.implicitly_wait(10)
+        time.sleep(3)
+
--- hw_5/tests/test_register.py
+++ hw_5/tests/test_register.py
@@ -1,25 +1,38 @@
 from selenium import webdriver
-from Pages.register_page import RegisterPage
+
+
+from hw_5.Pages.register_page import RegisterPage
 
 driver = webdriver.Chrome()
 register_page = RegisterPage(driver)
 
 
+
 def test_firstname_input():
+    register_page.move_my_account_dropdown()
     assert register_page.does_firstname_input_exist()
 
 
 def test_lastname_input():
+    register_page.move_my_account_dropdown()
     assert register_page.does_lastname_input_exist()
 
 
 def test_continue_button():
+    register_page.move_my_account_dropdown()
     assert register_page.does_continue_button_exist()
 
 
 def test_link_Privacy_Policy():
+    register_page.move_my_account_dropdown()
     assert register_page.does_link_Privacy_Policy_exist()
 
 
 def test_checkbox_Subscribe():
+    register_page.move_my_account_dropdown()
     assert register_page.does_checkbox_Subscribe_exist()
+
+# def test_move():
+#     register_page.move_my_account_dropdown()
+
+
