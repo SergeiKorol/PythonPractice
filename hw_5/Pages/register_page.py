3379cc3c121c1578605e659205248cf196bf6865
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-05-18 00:55:38 +0400

    1 Убрал Try\except 2 Добавил Док стринги

--- hw_5/Pages/register_page.py
+++ hw_5/Pages/register_page.py
@@ -5,20 +5,17 @@ import time
 
 
 class RegisterPage:
+    """
+    Класс для работы со страницей регистрация и элементами на этой странице
+    """
     url = "http://localhost"
-    #url = "http://localhost/index.php?route=account/register"
+
 
     # Локаторы
     firstname_input_locator = (By.XPATH, '//div/input[@placeholder="First Name"]')
     lastname_input_locator = (By.XPATH, '//div/input[@placeholder="Last Name"]')
-    #continue_button_locator = (By.XPATH, '//div[@class="text-end"]/button')
     link_Privacy_Policy_locator = (By.XPATH, '//div[@class="text-end"]/div/label')
     checkbox_Subscribe_locator = (By.XPATH, '//div/input[@type="checkbox"]')
-    #my_account_locator = (By.CSS_SELECTOR, '.d-none d-md-inline')
-    #my_account_locator = (By.XPATH, "//li/div/a[@class='dropdown-toggle']")
-    # my_account_locator = (By.CSS_SELECTOR, 'a.dropdown - toggle[data - bs - toggle = "dropdown"]')
-    # my_account_locator = (By.LINK_TEXT, "My Account")
-    # my_account_locator = (By.XPATH,'//a[@class="dropdown-toggle" and @data-bs-toggle="dropdown"]')
     my_account_locator = (By.XPATH, "//a[contains(@href,'route=account/account')]")
     continue_button_locator = (By.XPATH, "//button[@type='submit']")
     register_locator = (By.XPATH, '//a[@class="dropdown-toggle show" and @data-bs-toggle="dropdown" and @aria-expanded="true"]')
@@ -33,49 +30,48 @@ class RegisterPage:
 
     # Методы для проверки наличия элементов
     def does_firstname_input_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.firstname_input_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента firstname и получения его CSS стиля color
+        """
+        firstname_input = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.firstname_input_locator))
+        return firstname_input.value_of_css_property("color")
 
     def does_lastname_input_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.lastname_input_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента lastname и получения его CSS стиля color
+        """
+        lastname_input = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.lastname_input_locator))
+        return lastname_input.value_of_css_property("color")
 
     def does_continue_button_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.continue_button_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента continue_button и получения его CSS стиля color
+        """
+        continue_button = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.continue_button_locator))
+        return continue_button.value_of_css_property("color")
 
     def does_link_Privacy_Policy_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.link_Privacy_Policy_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента Privacy_Policy и получения его CSS стиля color
+        """
+        link_Privacy_Policy = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.link_Privacy_Policy_locator))
+        return link_Privacy_Policy.value_of_css_property("color")
 
     def does_checkbox_Subscribe_exist(self):
-        try:
-            WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.checkbox_Subscribe_locator))
-            return True
-        except Exception as e:
-            return e
+        """
+        Метод для проверки наличия элемента checkbox_Subscribe и получения его CSS стиля color
+        """
+        checkbox_Subscribe = WebDriverWait(self._driver, 2).until(EC.visibility_of_element_located(self.checkbox_Subscribe_locator))
+        return checkbox_Subscribe.value_of_css_property("color")
 
     def move_my_account_dropdown(self):
-        self._driver.implicitly_wait(10)
+        """
+        Метод для перехода на страницу Register
+        """
         my_account_link = self._driver.find_element(By.LINK_TEXT, "My Account")
         my_account_link.click()
-        self._driver.implicitly_wait(10)
-        time.sleep(3)
-        self._driver.implicitly_wait(10)
+        time.sleep(1)
         register_link = self._driver.find_element(By.LINK_TEXT, "Register")
         register_link.click()
-        self._driver.implicitly_wait(10)
-        time.sleep(3)
+        time.sleep(1)
 
--- hw_5/tests/test_main_page.py
+++ hw_5/tests/test_main_page.py
@@ -1,29 +1,53 @@
 from selenium import webdriver
-from Pages.main import MainPage
+from hw_5.Pages.main import MainPage
 
 driver = webdriver.Chrome()
 main_page = MainPage(driver)
 
 
 def test_search_field():
-    assert main_page.does_search_field_exist()
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(33, 37, 41, 1)'
+    assert main_page.does_search_field_exist() == expected_css
 
 
-def test_category_dropdown():
-    assert main_page.does_category_dropdown_exist()
+def test_desktop_dropdown():
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(35, 161, 209, 1)'
+    assert main_page.does_opencart_exist() == expected_css
 
 
 def test_button_total():
-    assert main_page.does_button_total_exist()
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(68, 68, 68, 1)'
+    assert main_page.does_featured_exist() == expected_css
 
 
 def test_footer():
-    assert main_page.does_footer_exist()
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(226, 226, 226, 1)'
+    assert main_page.does_footer_exist() == expected_css
 
 
 def test_wishlist():
-    assert main_page.does_wishlist_exist()
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(136, 136, 136, 1)'
+    assert main_page.does_wishlist_exist() == expected_css
 
 
 def test_currency_dropdown():
-    assert main_page.does_currency_dropdown_exist()
+    """
+    Тест проверяет что CSS стиль color соответствует ожидаемому в переменной expected_css
+    """
+    expected_css = 'rgba(204, 204, 204, 1)'
+    assert main_page.does_opencart_link_exist() == expected_css
